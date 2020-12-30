f0d6161d4422039c91eddd9889fc29e3f9cfc5b5
ASofrohan <asgoan@gmail.com>
2020-12-30 12:09:28 +0100

    Finished

--- src/org/ucm/tp1/Control/Commands/SaveCommand.java
+++ src/org/ucm/tp1/Control/Commands/SaveCommand.java
@@ -1,5 +1,6 @@
 package org.ucm.tp1.Control.Commands;
-
+import java.util.Scanner;
+import java.io.*;
 import org.ucm.tp1.Control.Exceptions.CommandExecuteException;
 import org.ucm.tp1.Control.Exceptions.CommandParseException;
 import org.ucm.tp1.Logic.Game;
@@ -9,9 +10,25 @@ public class SaveCommand extends Command {
 	public SaveCommand() {
 		super("save", "s", "[s]ave", "save the game state in a file.dat");
 	}
+	
 	@Override
 	public boolean execute(Game game) throws CommandExecuteException {
 		boolean refreshDisplay = false;
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("File name where you want to save the game:\n");
+		String fileName = scanner.nextLine();
+		fileName += ".dat";
+		File fileOut = new File(fileName);
+		try{
+		BufferedWriter outSource = new BufferedWriter(new FileWriter(fileOut));
+		String data = "Buffy the Vampire Slayer v3.0\n" + game.serialize();
+		outSource.write(data);
+		outSource.close();
+		System.out.print("Game successfully saved in file " + fileName + "\n");
+		}catch(IOException e) {
+			throw new CommandExecuteException("[ERROR]: Failed to create file " + fileName + "\n[ERROR]: Failed to save game");
+		}
 		return refreshDisplay;
 	}
 
