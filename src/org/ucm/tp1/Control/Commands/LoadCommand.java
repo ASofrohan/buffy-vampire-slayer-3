34b985fb3efd9ac8b4819a399de5f0d70416334d
ASofrohan <asgoan@gmail.com>
2021-02-06 19:01:19 +0100

    Load Command

---
+++ src/org/ucm/tp1/Control/Commands/LoadCommand.java
@@ -0,0 +1,56 @@
+package org.ucm.tp1.Control.Commands;
+
+import java.io.*;
+import org.ucm.tp1.Control.Exceptions.CommandExecuteException;
+import org.ucm.tp1.Control.Exceptions.CommandParseException;
+import org.ucm.tp1.Logic.Game;
+
+public class LoadCommand extends Command {
+	
+	String filename;
+	
+	public LoadCommand() {
+		super("load", "l", "[l]oad <filename>", "load the game state from a file.dat");
+	}
+	
+	public LoadCommand(String filename) {
+		super("load", "l", "[l]oad <filename>", "load the game state from a file.dat");
+		this.filename = filename;
+	}
+	
+	@Override
+	public boolean execute(Game game) throws CommandExecuteException {
+		boolean refreshDisplay = false;
+		filename += ".dat";
+		FileReader fr = null;
+		BufferedReader inSource = null;
+		try {
+			fr = new FileReader(filename);
+			inSource = new BufferedReader(fr);
+			System.out.println(inSource.read());
+		}catch(IOException e){
+			throw new CommandExecuteException("[ERROR]: Failed to read from file " + filename + "\n[ERROR]: Failed to load game");
+		}finally {
+			if(inSource != null) {
+				try{
+					inSource.close();
+				}catch (Exception ignore) {
+					//nothing to do
+				}
+			}
+		}
+		return refreshDisplay;
+	}
+
+	@Override
+	public Command parse(String[] commandWords) throws CommandParseException {
+		if(commandWords.length == 2 && matchCommandName(commandWords[0])) {			//return command with attributes
+			return new LoadCommand(commandWords[1]);
+		}
+		else if (commandWords.length == 1 && matchCommandName(commandWords[0])) {
+			throw new CommandParseException("[ERROR]: Command " + name + ": " + incorrectNumberOfArgsMsg);
+		}
+		return parseNoParamsCommand(commandWords);
+	}
+
+}
